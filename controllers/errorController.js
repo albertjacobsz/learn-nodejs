1434e4eb8891d200295f2874f88c210e85172952
albertjacobsz <dekater403@gmail.com>
2021-06-26 18:29:26 +0400

    LEC119 END

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -26,6 +26,12 @@ const handleCastErrorDB = (err) => {
   const message = `invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value ${value}, please use another value`;
+  return new AppError(message, 402)
+}
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -34,6 +40,7 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV) {
     let error = { ...err };
     if (error.name === 'castError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     sendErrorProd(error, res);
   }
 };
