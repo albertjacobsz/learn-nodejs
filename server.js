e8ee8e09a11d9a306767644c5c0663c8652fbacc
albertjacobsz <dekater403@gmail.com>
2021-06-27 15:46:28 +0400

    LEC122

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+process.on('uncaughtException', err => {
+  console.log('Uncaught exception, shutting down');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 // eslint-disable-next-line prettier/prettier
 dotenv.config({path: './config.env'});
 const DB = process.env.DATABASE.replace(
@@ -30,5 +35,4 @@ process.on('unhandledRejection', err => {
   server.close(() => {
     process.exit(1);
   });
-
-})
+});
